17656e9854c8c820bb2b5f78b57eb2ae139990d5
duckweed <andrew.oxenburgh@gmail.com>
2012-10-16 20:57:02 +1300

    boolean converter now accepts strings. better info in logging

--- src/fitnesse/slim/fixtureInteraction/LoggingInteraction.java
+++ src/fitnesse/slim/fixtureInteraction/LoggingInteraction.java
@@ -9,15 +9,25 @@ public class LoggingInteraction extends DefaultInteraction {
   public Object methodInvoke(Method method, Object instance, Object... convertedArgs) throws InvocationTargetException, IllegalAccessException {
     long startTime = System.nanoTime();
     Object o = super.methodInvoke(method, instance, convertedArgs);
-    System.out.println("methodInvoke : " + method.getName() + " = " + (System.nanoTime() - startTime));
+    long endTime = System.nanoTime();
+    System.out.println("methodInvoke : " + createTimestamp(method, startTime, endTime));
     return o;
   }
 
   @Override
-  public Object newInstance(Constructor<?> constructor, Object... initargs) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+  public Object newInstance(Constructor constructor, Object... initargs) throws InvocationTargetException, InstantiationException, IllegalAccessException {
     long startTime = System.nanoTime();
     Object o = super.newInstance(constructor, initargs);
-    System.out.println("newInstance : " + (System.nanoTime() - startTime));
+    long endTime = System.nanoTime();
+    System.out.println("newInstance : " + createTimestamp(constructor, startTime, endTime));
     return o;
+  }
+
+  private String createTimestamp(Method method, long startTime, long endTime) {
+    return method.getDeclaringClass() + "." + method.getName() + " = " + (endTime - startTime);
+  }
+
+  private String createTimestamp(Constructor method, long startTime, long endTime) {
+    return method.getDeclaringClass() + "." + method.getName() + " = " + (endTime - startTime);
   }
 }
